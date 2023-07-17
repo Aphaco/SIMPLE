94c3127d9c96eb02ece17140e6b661b27b53b975
Aphaco <136358631+Aphaco@users.noreply.github.com>
2023-07-17 12:57:37 +0000

    Add files via upload

---
+++ si_calc.py
@@ -0,0 +1,14 @@
+def interest(p, r, t):
+    print(p * r * t * 0.01)
+
+
+def principal(i, r, t):
+    print((100 * i) / (r*t))
+
+
+def rate(i, p, t):
+    print((100 * i) / (p * t))
+
+
+def time(r, i, p):
+    print((100 * i) / (p * r))
